c85338665bdabb71a964ef920bfce1cef70ecb1a
Armenia Cole <armenia.cole@hotmail.co.nz>
2019-08-27 18:27:37 -0400

    setup

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// Pseudo Code
+
+// Randomize function
+
+// Make ajax call for each category - store it in a variable
+
+// store ajax variables in an array
+
+// Randomize categories from that array
+
+// Event listener for button,
+    //  clear board(if content)
+    //  If turns available - randomized category(displays over wheel) and question(displays in question interface) form api(ajax call is filtered to limit results).
+    //  Take a turn away
+
+
+// Event listener for question answers - if true deliver message and add score, else just deliver message.
+    // When no turns available, 
+    // game over.Display score(pop up has a try again button) 
+    
+
