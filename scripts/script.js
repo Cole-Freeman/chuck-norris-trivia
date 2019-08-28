ce80fd3ad568026283a88810c707eb2e0ad7407f
Armenia Cole <armenia.cole@hotmail.co.nz>
2019-08-28 09:51:11 -0400

    axaj calls

--- scripts/script.js
+++ scripts/script.js
@@ -17,5 +17,86 @@
 // Event listener for question answers - if true deliver message and add score, else just deliver message.
     // When no turns available, 
     // game over.Display score(pop up has a try again button) 
-    
+  
+const spinApp = {};
+
+spinApp.celebs = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=26&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function(result){
+    // console.log(result);
+})
+
+spinApp.sport = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function (result) {
+    // console.log(result);
+})
+
+spinApp.geo = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=22&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function (result) {
+    // console.log(result);
+})
+
+spinApp.animals = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=27&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function (result) {
+    // console.log(result);
+})
+
+spinApp.science = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=17&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function (result) {
+    // console.log(result);
+})
+
+spinApp.tv = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=14&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function (result) {
+    // console.log(result);
+})
+
+spinApp.music = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=12&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function (result) {
+    // console.log(result);
+})
+
+
+spinApp.film = $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&category=11&difficulty=easy&type=multiple",
+    type: 'GET',
+    dataType: 'json',
+}).then(function (result) {
+    // console.log(result);
+})
+
+spinApp.categories = [spinApp.animals, spinApp.celebs, spinApp.geo, spinApp.music, spinApp.science, spinApp.sport, spinApp.tv, spinApp.film];
+
+console.log(spinApp.categories);
+
+spinApp.randomCategory = function (array) {
+    const randomNumber = Math.floor(Math.random() * this.categories.length);
+    return array[randomNumber];
+}
+
+
+$(function(){
+    randomCategory(spinApp.categories);
+});
+
 
