aac6a276707f4beeffdf189bbda7b40a73e1756e
Armenia Cole <armenia.cole@hotmail.co.nz>
2019-08-30 09:29:45 -0400

    mvp tidy up

--- scripts/script.js
+++ scripts/script.js
@@ -1,93 +1,5 @@
-// Pseudo Code
-
-// Randomize function
-
-// Make ajax call for each category - store it in a variable
-
-// store ajax variables in an array
-
-// Randomize categories from that array
-
-// Event listener for button,
-    //  clear board(if content)
-    //  If turns available - randomized category(displays over wheel) and question(displays in question interface) form api(ajax call is filtered to limit results).
-    //  Take a turn away
-
-
-// Event listener for question answers - if true deliver message and add score, else just deliver message.
-    // When no turns available, 
-    // game over.Display score(pop up has a try again button) 
   
 const spinApp = {};
-
-
-// // Ajax calls for the different Categories 
-// spinApp.celebs = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=26&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then(function(result){
-//     return result.results[0];
-// })
-
-
-// spinApp.sport = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then(function (result) {
-//     return result.results[0];
-// })
-
-// spinApp.geo = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=22&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then(function (result) {
-//     // console.log(result)
-//     return result.results[0];
-// })
-
-// spinApp.animals = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=27&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then(function (result) {
-//     return result.results[0];
-// })
-
-// spinApp.science = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=17&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then(function (result) {
-//     return result.results[0];
-// })
-
-// spinApp.tv = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=14&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then(function (result) {
-//     return result.results[0];
-// })
-
-// spinApp.music = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=12&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then((result) => {
-//     return result.results[0];
-// })
-
-
-// spinApp.film = $.ajax({
-//     url: "https://opentdb.com/api.php?amount=1&category=11&difficulty=easy&type=boolean",
-//     type: 'GET',
-//     dataType: 'json',
-// }).then(function (result) {
-//     return result.results[0];
-// })
 
 spinApp.singleAjaxCall = function(categoryId){
    return $.ajax({
@@ -105,20 +17,17 @@ spinApp.singleAjaxCall = function(categoryId){
     })
 }
 
-
-// // Array of categories for randomizing (promises)
-spinApp.categories = [spinApp.animals, spinApp.celebs, spinApp.geo, spinApp.music, spinApp.science, spinApp.sport, spinApp.tv, spinApp.film];
-
 spinApp.cat = [ 
     {category: "animals", id: 27},
     {category: "celebs", id: 22},
-    { category: "geo", id: 21 },
-    { category: "music", id: 12 },
-    { category: "science", id: 17 },
-    { category: "sport", id: 26 },
-    { category: "tv", id: 14 },
-    { category: "film", id: 11 },
+    {category: "geo", id: 21},
+    {category: "music", id: 12},
+    {category: "science", id: 17},
+    {category: "sport", id: 26},
+    {category: "tv", id: 14},
+    {category: "film", id: 11},
     ];
+
 // Randomize function
 spinApp.randomCategory = function (array) {
     const randomNumber = Math.floor(Math.random() * this.cat.length);
@@ -130,10 +39,8 @@ let answerObjectSave = "";
 spinApp.spinnerClick = function (){
 
     $("button").on("click", function () {
-        console.log("clicked");
 
         const turnsLeft = $(".turns").text();
-        // console.log (turnsLeft);
 
         $(".quiz").html(``);
 
@@ -143,7 +50,6 @@ spinApp.spinnerClick = function (){
             const success = spinApp.singleAjaxCall(selectedCat.id);
         
             $.when(success).then((answerObject) => {
-                // console.log(answerObject);
                 
                 $(".quiz").html(`<h2>${answerObject.category}</h2><p>${answerObject.question}</p>
                 <form action="#">
@@ -166,8 +72,6 @@ spinApp.spinnerClick = function (){
             // switch this to sweet alert
             if(!alert('Game Over! Play again?')){window.location.reload();}
         }
-       
-        // setTimeout(function () { console.log(spinApp.singleAjaxCall(17)); }, 1000)
 
         $(".verdict").html(``)
 
@@ -176,67 +80,32 @@ spinApp.spinnerClick = function (){
     });
 }
 
-console.log(answerObjectSave);
-
 spinApp.submitButton = function () {
-$("form").on("submit", function (event) {
-    event.preventDefault();
+    $("form").on("submit", function (event) {
+        event.preventDefault();
 
-    const userAnswer = $(`input[name=answers]:checked`).val();
-    const ajaxAnswer = answerObjectSave.correct_answer;
-    const incorrectAnswer = answerObjectSave.incorrect_answers;
-    console.log(answerObjectSave);
-    console.log(incorrectAnswer);
-    console.log(userAnswer);
-    
+        const userAnswer = $(`input[name=answers]:checked`).val();
+        const ajaxAnswer = answerObjectSave.correct_answer;
+        const incorrectAnswer = answerObjectSave.incorrect_answers;
 
-    // $("form").validate();
+        if (ajaxAnswer === userAnswer) {
+            $(".verdict").html(`<p>That's right!  You get 5 points!</p>`)
+            $('.score').text(Number($(".score").text())+5);
 
-    if (ajaxAnswer === userAnswer) {
-        $(".verdict").html(`<p>That's right!  You get 5 points!</p>`)
-        $('.score').text(Number($(".score").text())+5);
+        } else if (incorrectAnswer == userAnswer) {
+            $(".verdict").html(`<p>Incorrect Answer</p>`)
+            
+        }  else {
+            $(".verdict").html(`<p>Select an answer!</p>`)
+        }
 
-    } else if (incorrectAnswer == userAnswer) {
-        $(".verdict").html(`<p>Incorrect Answer</p>`)
-        
-    }  else {
-        $(".verdict").html(`<p>Select an answer!</p>`)
-    }
-
-
-});
+    });
 
 }
 
-
-
-
-
 $(function(){
-    // these ... are both spreads
-    // $.when(...spinApp.categories).then((...res) =>{
-    //     spinApp.categories = res;
-    //     randomResult = spinApp.randomCategory(spinApp.categories);
-    //     // console.log(res, spinApp.categories);
-    // });
     
     spinApp.spinnerClick();
     spinApp.submitButton();
-    // spinApp.nextButton();
 
 });
-
-
-// issues on issues on issues
-
-//  will need to randomise the location of the answer so it doesn't always display the same
-
-// push all possible answers into array and randomise from array. loop to display.
-// question only updates when page is refreshed?? Other data need to stay- might need to use firebase to save user data and then force a refresh after each spin
-// he.js decoder needs to run on question??
-
-
-// for true or false we need 
-// display radio buttons with value of true or false to screen. 
-// check value of radio with if else statement
-// why is form submitting???
